ef9b653208c82848cf67e42e1a347b68bbf47dec
Athaariq Ardhiansyah <athaariqa@gmail.com>
2020-09-08 01:30:58 +0700

    Updates

---
+++ precache-manifest.4741784d9c24b252bde09d154eb42997.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9da89b9c0a91f38f86a9988c419a7ac",
+    "url": "/my-portofolio/index.html"
+  },
+  {
+    "revision": "57b4070d4d8631cb20fe",
+    "url": "/my-portofolio/static/css/main.1f9ab70b.chunk.css"
+  },
+  {
+    "revision": "cb23e7b7ab18ad3cf191",
+    "url": "/my-portofolio/static/js/2.864bed40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-portofolio/static/js/2.864bed40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b4070d4d8631cb20fe",
+    "url": "/my-portofolio/static/js/main.8b1a4b9e.chunk.js"
+  },
+  {
+    "revision": "cd902166c5e4df27c7a7",
+    "url": "/my-portofolio/static/js/runtime-main.43dd6cc9.js"
+  },
+  {
+    "revision": "9062d79915fc16248038c1077cc86781",
+    "url": "/my-portofolio/static/media/computer_icon.9062d799.svg"
+  },
+  {
+    "revision": "33af9581beb2dd837e0885423a9b0c45",
+    "url": "/my-portofolio/static/media/fiwl.33af9581.svg"
+  },
+  {
+    "revision": "b4129d3dcefaffdbcb2b8c8edeb147b4",
+    "url": "/my-portofolio/static/media/geek.b4129d3d.svg"
+  },
+  {
+    "revision": "2aaebcd4f70b9a942a86caf674d25c8d",
+    "url": "/my-portofolio/static/media/header.2aaebcd4.jpg"
+  },
+  {
+    "revision": "6817834eac22f905e5a3fa61efae0f0f",
+    "url": "/my-portofolio/static/media/its_campus.6817834e.jpg"
+  },
+  {
+    "revision": "faac72e56fe509c6448deac897319d26",
+    "url": "/my-portofolio/static/media/key.faac72e5.svg"
+  },
+  {
+    "revision": "425b92c16c780f4149633a6f15fd87c2",
+    "url": "/my-portofolio/static/media/profile.425b92c1.jpg"
+  },
+  {
+    "revision": "7b2a7754a4d9dd6fb199e7c00c97365e",
+    "url": "/my-portofolio/static/media/quote.7b2a7754.svg"
+  },
+  {
+    "revision": "19e18b258c87581981a5fb5308f7babd",
+    "url": "/my-portofolio/static/media/sand.19e18b25.jpg"
+  },
+  {
+    "revision": "ce999a4cce80757bef6f1b473f5a91ef",
+    "url": "/my-portofolio/static/media/sleepy_passenger.ce999a4c.svg"
+  }
+]);
